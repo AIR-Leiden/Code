93e1035f72653a96a3b7d60c5bb0ea6802e6a1a7
Michiel van der Meer <michielmeer@live.nl>
2023-10-04 16:07:36 +0200

    Tutorial package comments

--- air_challenge/scripts/move.py
+++ air_challenge/scripts/move.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""
+We will move your robot!
+
+Make sure the robot is on the ground, there are no cables around and that there is nothing in front
+of the robot!
+"""
 
 from datetime import datetime
 
 import rospy
 from geometry_msgs.msg import Twist
 
-msg = """
-We will move your robot!
-
-Make sure there is nothing in front of the robot.
-"""
 
 def check_stop(start_time, seconds=2):
     """
-    Check if the robot should stop depending on the time elapsed in milliseconds (ms)
+    Check if the robot should stop depending on the time elapsed in seconds.
     """
     now = datetime.now()
     delta = now - start_time
@@ -23,25 +24,30 @@ def check_stop(start_time, seconds=2):
 
 if __name__=="__main__":
 
+    # Create a ROS node for our package
     rospy.init_node('air_challenge')
+    # Our package does not rely on any input information (it uses time), we output wheel command
+    # messages. We register a publisher for the appropriate topic and message type.
     pub = rospy.Publisher('~cmd_vel', Twist, queue_size=5)
 
+    # Our robot has a fixed speed
     speed = 0.2 # m/s
-    turn = 0.5 # rad/s
 
     start_time = datetime.now()
     print("Starting at {}".format(start_time))
+
     try:
+        # Keep driving for a predefined period of time by repeatedly publishing the move message
         while not check_stop(start_time):
             print("  Moving forward!")
             twist = Twist()
-            twist.linear.x  = speed;
-            # twist.angular.z = turn;
+            twist.linear.x = speed;
             pub.publish(twist)
 
     except Exception as e:
         print(e)
 
+    # On exit, make sure we stop the robot
     finally:
         print("Stopping..")
         twist = Twist()
